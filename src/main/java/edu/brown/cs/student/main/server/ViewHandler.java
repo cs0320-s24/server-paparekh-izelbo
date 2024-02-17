0f2521838d55f61fc65d9b38547c46c60ef3aec6
Ian Zelbo <113198991+izelbo129@users.noreply.github.com>
2024-02-17 01:38:05 -0500

    Feat: added viewhandler

--- src/main/java/edu/brown/cs/student/main/server/ViewHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/ViewHandler.java
@@ -1,5 +1,65 @@
 package edu.brown.cs.student.main.server;
 
-public class ViewHandler {
+import edu.brown.cs.student.main.interfaces.Creator;
+import edu.brown.cs.student.main.search.Parser;
+import edu.brown.cs.student.main.search.Search;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class ViewHandler implements Route {
+
+  private final LoadHandler loadHandler;
+
+  public ViewHandler(LoadHandler loadHandler) {
+    this.loadHandler = loadHandler;
+
+  }
+
+  @Override
+  public Object handle(Request request, Response response) throws Exception {
+    Map<String, Object> responseMap = new HashMap<>();
+
+    try {
+
+      //So we don't have to parse more than once
+
+      Parser parser = this.loadHandler.getParser();
+
+      if (parser == null) {
+        responseMap.put("result", "You have not loaded your file yet.");
+        return responseMap;
+      }
+
+      List<String[]> elements = parser.getValues();
+
+      String serialized = Serialization.convertToJson(listToArray(elements));
+
+      responseMap.put("result", "success");
+      responseMap.put("data", serialized);
+
+    } catch (Exception e) {
+      responseMap.put("result", "error");
+    }
+
+    //success?
+    return responseMap;
+
+  }
+
+
+  public String[][] listToArray(List<String[]> list) {
+    String[][] array = new String[list.size()][];
+    for (int i = 0; i < list.size(); i++) {
+      array[i] = list.get(i);
+    }
+    return array;
+  }
 
 }
